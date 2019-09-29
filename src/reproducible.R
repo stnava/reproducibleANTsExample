90f6686d65f0d315dd7e60762331839f9a6639aa
stnava <stnava@gmail.com>
2019-09-29 08:09:17 -0400

    ENH: python

--- src/reproducible.R
+++ src/reproducible.R
@@ -6,9 +6,9 @@ metrics = c( 'MeanSquares','GC',"Mattes")
 for ( metric in metrics ) {
   mytx1 <- antsRegistration(fixed=fi, mi, typeofTransform = tx, affMetric=metric )
   p1=getAntsrTransformParameters( readAntsrTransform( mytx1$fwdtransforms[2] ))
-  for ( k in 1:1 ) {
+  for ( k in 1:2 ) {
     mytx2 <- antsRegistration(fixed=fi, mi, typeofTransform = tx, affMetric=metric  )
     p2=getAntsrTransformParameters( readAntsrTransform( mytx2$fwdtransforms[2] ))
-    print( paste( metric, "Change", sum( abs( p1 - p2 ) ), "Similarity", antsImageMutualInformation( fi,mytx2$warpedmovout) ) )
+    print( paste( metric, "Change: try", k, "Is", sum( abs( p1 - p2 ) ), "Similarity=", antsImageMutualInformation( fi,mytx2$warpedmovout) ) )
   }
 }
